cb19a0a5ae6f3f935856947e07b182615d90c140
SSHari <s-hari@live.com>
2018-06-05 05:08:05 +0000

    add express ejs and views for landing page and topics page

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('landing');
+});
+
+app.get('/topics', function(req, res) {
+    res.render('topics');
+});
+
+app.get('*', function(req, res) {
+    res.send('404 Not Found...Try Looking Somewhere Else!');
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('The CodeSurf Server Has Started!');
+});
