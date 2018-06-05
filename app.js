42ecf463367103089c4962036331567bc334e0ff
SSHari <s-hari@live.com>
2018-06-05 06:03:22 +0000

    add bootstrap elements to topics index page

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var express = require('express'),
-    app = express();
+	app = express();
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('landing');
+	res.render('landing');
 });
 
 app.get('/topics', function(req, res) {
-    res.render('topics');
+	res.render('topics');
 });
 
 app.get('*', function(req, res) {
-    res.send('404 Not Found...Try Looking Somewhere Else!');
+	res.send('404 Not Found...Try Looking Somewhere Else!');
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
-    console.log('The CodeSurf Server Has Started!');
+	console.log('The CodeSurf Server Has Started!');
 });
